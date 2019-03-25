e684f464fe245cbcc253110f436a9e5fa2cf2550
xander0627 <xander0627@outlook.com>
2019-03-25 16:50:48 +0800

    first commit

---
+++ src/components/LeftMenu.vue
@@ -0,0 +1,145 @@
+<template>
+    <transition name="slide-fade">
+    <div class="menu" v-if="isMenu" @click="isMenu = !isMenu">
+        <div class="left_menu" @click.stop="">
+            <section class="person_message">
+                <div class="head">
+                    <img src="../../static/img/movie_default.png" alt="">
+                </div>
+                <div class="datum">
+                    <h2 class="name">周星驰</h2>
+                    <p class="Member_Level">LV2<span>普通用户</span></p>
+                </div>
+            </section>
+            <ul class="menu_list">
+                <li>想看列表</li>
+                <li>看过列表</li>
+                <li>社区</li>
+                <li>帮助与反馈</li>
+                <li>联系</li>
+                <li>关于</li>
+            </ul>
+            <router-link to="/register" class="drop_out" @click="isMenu = !isMenu">退出登录</router-link> 
+        </div>
+    </div>
+    </transition>
+    
+</template>
+<script>
+    import bus from '../../static/js/bus'
+    export default {
+        name:'leftMenu',
+        data(){
+            return {
+                isMenu:false
+            }
+        },
+        components:{
+
+        },
+        created() {
+            bus.$on('showMenu' ,(data) => {
+                this.isMenu = data
+            })
+            
+        },
+    }
+</script>
+<style scoped>
+    .menu{
+        position: fixed;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        top: 0;
+        background-color: rgba(0,0,0,.3);
+        z-index: 999;
+    }
+    .left_menu{
+        position: relative;
+        width: 5rem;
+        height: 100%;
+        background-color: #FFF;
+    }
+    .person_message{
+        padding:.6rem 0 .3rem .6rem;
+        display: flex;
+        align-items:center;
+    }
+    .person_message .head{
+        width: 1.2rem;
+        height: 1.2rem;
+        border-radius: 50%;
+        margin-right: .3rem;
+        border: 2px solid #259faf;
+        box-sizing: content-box;
+        overflow: hidden;
+    }
+    .person_message .head img{
+        width: 100%;
+        height: 100%;
+    }
+    .person_message .name{
+        font-size: .32rem;
+        font-weight: normal;
+        color: #333;
+        margin-bottom: .1rem;
+    }
+    .person_message .Member_Level{
+        color: #259faf;
+        display: flex;
+        align-items: center;
+    }
+    .person_message .Member_Level span{
+        font-size: .24rem;
+        color: #259faf;
+        padding: .02rem .2rem;
+        border: 1px solid #259faf;
+        border-radius: 20px;
+        margin-left: .1rem;
+    }
+    .menu_list{
+        padding-left: .6rem;
+    }
+    .menu_list li{
+        position: relative;
+        padding: .3rem 0;
+    }
+    .menu_list li::after{
+        content: " ";
+        position: absolute;
+        top: 50%;
+        right:.6rem;
+        margin-top:-.15rem;
+        width: .3rem;
+        height: .3rem;
+        transform: rotate(180deg);
+        -o-transform: rotate(180deg);
+        -webkit-transform: rotate(180deg);
+        -moz-transform: rotate(180deg);
+        background-image: url('../../static/img/back.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+    }
+    .drop_out{
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        padding: .3rem 0 .3rem .6rem;
+        color: #259faf;
+    }
+    .drop_out::after{
+        display: none;
+    }
+    .slide-fade-enter-active {
+        transition: all .3s ease;
+    }
+    .slide-fade-leave-active {
+        transition: all .3s cubic-bezier(1.0, 0.2, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to{
+        transform: translateX(-7rem);
+    }
+</style>
---
+++ src/page/Home.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="home">
+        <header-top :showState="{ispageUp:true,isTitle:true,isSearchIcon:true}"></header-top>
+        <left-menu></left-menu>
+        <keep-alive>
+            <component :is="changeName"></component>
+        </keep-alive>
+        <footer-nav v-on:changeView="changeLink"></footer-nav>
+    </div>
+</template>
+
+<script>
+    import HeaderTop from '@/components/Header'
+    import FooterNav from '@/components/Footer'
+    import LeftMenu from '@/components/LeftMenu'
+    import Index from '@/page/Index'
+    import Showing from '@/page/Showing'
+    import RankingList from '@/page/RankingList'
+
+    export default {
+        name:'home',
+        data () {
+            return {
+                changeName:'index',
+                isMenu:null
+            }
+        },
+        components: {
+            HeaderTop,FooterNav,Index,Showing,RankingList,LeftMenu
+        },
+        created () {
+           
+        },
+        methods:{
+            changeLink(val){
+                this.changeName = val
+                let scrollTop = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset
+                if(scrollTop > 0){
+                    document.body.scrollTop = 0
+                    document.documentElement.scrollTop = 0
+                    window.pageYOffset = 0
+                }
+            }
+        },
+    }
+</script>
+<style scoped>
+    
+</style>
